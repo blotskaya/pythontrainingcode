bf0f6a9beb001304281a535aa04731ebadccd219
a.blotskaya <a.blotskaya@start.ru>
2024-02-03 21:48:26 +0300

    Text of exercises

---
+++ exercises/camelcases.py
@@ -0,0 +1,8 @@
+#Заполните решение так, чтобы функция разбивала верблюжью нотацию, используя пробел между словами.
+#Пример:
+#Input: "camelCasing" Output: "camel Casing"
+#Input: "identifier" Output: "identifier"
+
+def camel_casing():
+    pass
+
